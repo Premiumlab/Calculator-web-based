dabf70ff8c922da2ff061ee74fff30f5b43edad7
Sabirat <rubya001@umn.edu>
2017-10-27 13:27:49 -0500

    first commit

---
+++ client/src/Root.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+var amazingInlineJsStyle = {
+  border: '1px solid purple',
+  padding: '10px',
+  backgroundColor: '#ffffcc'
+}
+
+class RootContainer extends Component {
+  constructor() {
+    super();
+    this.state = { message: '' }
+  }
+
+  componentDidMount() {
+    var self = this;
+    axios.get('/api/example')
+    .then(function (response) {
+      console.log(response.data);
+      self.setState({message: response.data.message});  /*this will cause an invoke of the render() function again */
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+  /* Every time we change the value of a state variable, the render() function is called. That's why we need to make sure in the render() of
+  the Root class below that we received the prop 'messageFromServer' (see the if-else condition in the Root class)*/
+  render() {
+    return (
+      <Root messageFromServer={this.state.message} />
+    );
+  }
+}
+
+class Root extends Component {
+
+  render() {
+    console.log("this will print twice");
+    if(this.props.messageFromServer)
+    {
+      return (
+          <div className="Root">
+            <p>
+              Message from server:
+              <span style={amazingInlineJsStyle}>{this.props.messageFromServer}</span>
+            </p>
+            <p>
+              Root! <Link to={`/foo`}>a link to foo</Link>
+            </p>
+          </div>);
+    }
+    else {
+      return null;
+    }
+
+  }
+}
+
+export default RootContainer;
